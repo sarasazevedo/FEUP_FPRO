821b61edd7fd865272d0ab27d0ba03355e63bb95
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 19:04:01 +0000

    Update exercise4.py

--- P05/exercise4.py
+++ P05/exercise4.py
@@ -1,62 +1,19 @@
-'''Write a function mastermind(g1, g2, g3, c1, c2, c3) to evaluate a single line of a mastermind game.
+'''A possible representation of a multi-graph is as a tuple of pairs, each representing a single edge. For example, the multi-graph of the figure would be represented as (('A','B'), ('A','C'), ('B','C'), ('C','B'), ('A','B')).
 
-The function receives 6 single character strings. Each string can be "b" for blue, "w" for white and "y" for yellow. The first 3 arguments are the user guess. The last 3 arguments are the correct key. Argument g1 is the user guess for the value at argument c1 and so on.
+A more succinct representation would be as a tuple of triples, each representing a multi-edge associated with a multiplicity. The same graph would then be represented as (('A',2,'B'), ('A',1,'C'), ('B',1,'C'), ('C',1,'B')).
 
-You should give 3 points for each user guess that is completely correct, that is, the same color at the same position in the key and 1 point if the user guessed the color right but at the wrong position (that is, the color exists in the key but at another wrong position).  See the tests for examples.
+Write a Python function multi(g) that given a multi-graph g as a tuple of pairs, returns its representation as a tuple of triples with multiplicities. The order of triples in the tuple is irrelevant.'''
 
-Note that one right guess (position or color) counts only once.
+def multi(g):
+    multi = {}
+    for i in g:
+        if i in multi:
+            multi[i] += 1
+        else:
+            multi[i] = 1
 
-Por exemplo:
+    triples = [(i[0], multi[i], i[1]) for i in multi]
 
-Teste	Resultado
-print(mastermind("b", "w", "y", "b", "w", "y"))
-9
-print(mastermind("b", "b", "y", "b", "w", "b"))
-4
-print(mastermind("b", "w", "y", "w", "y", "w"))
-2
-print(mastermind("b", "b", "y", "y", "y", "b"))
-2'''
-def mastermind(g1, g2, g3, c1, c2, c3):
-    points = 0
-    
-    if (g1 == c1):
-        points += 3
-        g1 = "usada"
-        c1 = "usada"
-        
-    if (g2 == c2):
-        points += 3
-        g2 = "usada"
-        c2 = "usada"
-        
-    if (g3 == c3):
-        points += 3
-        g3 = "usada"
-        c2 = "usada"
-        
-    if ((g1 == c2) and (c2 != "usada") and (g2 != "usada")):
-        points += 1
-        c2 = "usada"
-        
-    elif ((g1 == c3) and (c3 != "usada") and (g3 != "usada")):
-        points += 1
-        c3 = "usada"
-        
-    if ((g2 == c1) and (c1 != "usada") and (g1 != "usada")):
-        points += 1
-        c1 = "usada"
-        
-    elif ((g2 == c3) and (c3 != "usada") and (g3 != "usada")):
-        points += 1
-        c3 = "usada"
-        
-    if ((g3 == c1) and (c1 != "usada") and (g1 != "usada")):
-        points += 1
-        c1 = "usada"
-        
-    elif ((g3 == c2) and (c2 != "usada") and (g2 != "usada")):
-        points += 1
-        c2 = "usada"
-        
-    return points
+    return tuple(triples)
+
+
