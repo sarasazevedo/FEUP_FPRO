7609dab73059f7f73ae5f538d9e1d4cfa69f9610
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 18:30:33 +0000

    Update exercise3.py

--- P02/exercise3.py
+++ P02/exercise3.py
@@ -1,3 +1,7 @@
+'''Convert the following diagram to computer code in Python.
+
+The operations should be done sequentially. Be aware of the precedence of the operations and that operands higher up in the diagram have precedence over operands lower down. (See figure)'''
+
 number1 = int(input())
 number2 = int(input())
 number3 = int(input())
