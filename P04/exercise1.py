dbcdcd3adb3565fff0cf994c9175dd61f75b8a85
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 19:31:24 +0000

    Update exercise1.py

--- P04/exercise1.py
+++ P04/exercise1.py
@@ -1,14 +1,6 @@
-'''Write a Python function calc_triangular(n) that receives a positive integer n and calculates the nth triangular number. The nth triangular number (Tn) is the number of dots in the triangular arrangement with n dots on each side, and is equal to the sum of the n natural numbers from 1 to n (https://en.wikipedia.org/wiki/Triangular_number):
-
-Tn=∑nk=1k=1+2+3+…+n 
-print(calc_triangular(1))
-1
-print(calc_triangular(3))
-6
-print(calc_triangular(10))
-55
-print(calc_triangular(23))
-276
+'''Write a Python function calc_triangular(n) that receives a positive integer n and calculates the nth triangular number. 
+The nth triangular number (Tn) is the number of dots in the triangular arrangement with n dots on each side, and is equal to the sum of the n natural numbers from 1 to n (https://en.wikipedia.org/wiki/Triangular_number):
+Tn=∑nk=1k=1+2+3+…+n
 '''
 
 def calc_triangular(n):
