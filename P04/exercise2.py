1a3913cb607ab0286997973d6abbdf0d27e1cd02
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 19:31:46 +0000

    Update exercise2.py

--- P04/exercise2.py
+++ P04/exercise2.py
@@ -1,19 +1,5 @@
 '''
 Write a Python function adigits(a, b, c) that receives three integers, each one with a single decimal digit and returns the lowest integer number that can be assembled with the three digits given as parameters.
-print(adigits(1, 2, 3))
-123
-print(adigits(9, 1, 9))
-199
-print(adigits(9, 1, 8))
-189
-print(adigits(0, 1, 0))
-1
-'''
-
-
-'''a = int(input("What's the number? "))
-b = int(input("What's the number? "))
-c = int(input("What's the number? "))
 '''
 def adigits(a, b, c):
     if a <= b and a <= c:
@@ -36,5 +22,3 @@ def adigits(a, b, c):
             b = b * 10
 
     return a + b + c
-
-''' print(adigits(a, b, c)) ''' 
