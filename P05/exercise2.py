cd33df18431a0666382b26df82548cf8c1b085b9
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 19:03:19 +0000

    Update exercise2.py

--- P05/exercise2.py
+++ P05/exercise2.py
@@ -1,40 +1,17 @@
-'''
-Write a Python function adigits(a, b, c) that receives three integers, each one with a single decimal digit and returns the lowest integer number that can be assembled with the three digits given as parameters.
-print(adigits(1, 2, 3))
-123
-print(adigits(9, 1, 9))
-199
-print(adigits(9, 1, 8))
-189
-print(adigits(0, 1, 0))
-1
-'''
+'''Write a Python function camel_case(phrase) to convert a given string phrase to the camelCase format.
+Camel case (sometimes stylized as camelCase or CamelCase, also known as camel caps or more formally as medial capitals) is the practice of writing phrases without spaces or punctuation, indicating the separation of words with a single "uppercase" (capital) letter, and the first letter always "lowercase" (minuscule).'''
+import string
 
-
-'''a = int(input("What's the number? "))
-b = int(input("What's the number? "))
-c = int(input("What's the number? "))
-'''
-def adigits(a, b, c):
-    if a <= b and a <= c:
-        a = a * 100
-        if b <= c:
-            b = b * 10
+def camel_case(pharase):
+    result = ""
+    upper = False
+    for ch in pharase: 
+        if(ch.isalpha()):
+            if upper:
+                result += ch.upper()
+                upper = False
+            else:
+                result += ch.lower()
         else:
-            c = c * 10
-    elif b <= a and b <= c:
-        b = b * 100
-        if a <= c:
-            a = a * 10
-        else:
-            c = c * 10
-    else:
-        c = c * 100
-        if a <= b:
-            a = a * 10
-        else:
-            b = b * 10
-
-    return a + b + c
-
-''' print(adigits(a, b, c)) ''' 
+            upper=True
+    return result
