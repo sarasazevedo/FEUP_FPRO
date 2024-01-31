f6636c3c22518434e6719df51a876d921b161ee2
Sara Azevedo <91210939+sarasazevedo@users.noreply.github.com>
2024-01-31 19:04:16 +0000

    Update exercise5.py

--- P05/exercise5.py
+++ P05/exercise5.py
@@ -1,43 +1,14 @@
-'''Write a Python function deriv(f) that receives a function f and returns a new function that computes df(x)
- numerically using the approximation below. Use round to approximate the result to 3 decimal places.
+'''Given a tuple of size > 3, write a Python function triplet(tup) that finds a triplet (a, b, c) where a, b, c are elements of tup, such that their sum is zero (a + b + c = 0).
 
-Recall that the derivative df(x)
- of a real function f(x)
- is defined as a limit:
+In case there is more than one triplet that sums up to zero, return the first occurrence when searching by lexicographical order of indexes. In case there are none, return an empty tuple, ().'''
 
-df(x)=limh→0f(x+h)−f(x)h
-
-For a small h, we can numerically compute the derivative using the equation:
-
-df(x)=f(x+h)−f(x)h,
-
-For the sake of simplicity, consider h=0.001
-.
-
-Por exemplo:
-
-Teste	Resultado
-def f(x): 
-    return x*x + 2*x + 2
-print(deriv(f)(3))
-8.001
-import math
-def g(x):
-    return math.sin(x) / x
-print(deriv(g)(3.14))
--0.319
-import math
-def f(x):
-    return math.log(x+10)
-print(deriv(f)(-5))
-0.2
-def f(x):
-    return 3/x + 5
-print(deriv(f)(55.231))
--0.001
-'''
-def deriv(f):
-    h = 0.001
-    def df(x):
-        return round((f(x+h)-f(x))/h,3)
-    return df 
+def triplet(tup): 
+    for i in range(len(tup)):
+        for j in range(i+1, len(tup)):
+            for k in range(j+1, len(tup)):
+                if tup[i] + tup[j] + tup[k] == 0:
+                    x = tup[i]
+                    y = tup[j]
+                    z = tup[k]
+                    return (x,y,z)
+    return ()
